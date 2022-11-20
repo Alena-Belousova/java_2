ca689f4421dcff53a6ca34030dea3a37ff5b3947
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:44:05 +0300

    преобразовать строку в массив

---
+++ Task39.java
@@ -0,0 +1,10 @@
+public class Task39  {
+    public static void main(String[] args) {
+
+    System.out.println("sem_2");
+
+    System.out.println("#39");
+    String str = new String("преобразовать строку в массив");
+    System.out.print("ответ: ");
+    System.out.println(str.toCharArray());
+}}
