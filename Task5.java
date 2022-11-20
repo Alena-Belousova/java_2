8e2a0101ca3d5f57641b3aa8a0f20053c7ccc0a8
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:35:00 +0300

    Метод concat

---
+++ Task5.java
@@ -0,0 +1,8 @@
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("sem_2");
+        System.out.print("#5"+'\t');
+        String str1= "Метод ";
+        String str2= "concat";
+        System.out.println(str1.concat(str2));
+}}
