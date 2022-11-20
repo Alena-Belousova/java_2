8651fbdae6b0d070f60b6cb31dfc7b913fe3d905
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:31:12 +0300

    Метод нахождения символа в строке по его индексу

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+public static void main(String[] args) {
+
+
+    System.out.println("sem_2");
+
+    System.out.print("#1"+'\t');
+    String a = "Метод нахождения символа в строке по его индексу";
+    char r1 = a.charAt(12);
+    char r2 = a.charAt(6);
+    System.out.println("Двенадцатый символ строки - " + r1);
+    System.out.println("Шестой символ строки - " + r2);
+}}
