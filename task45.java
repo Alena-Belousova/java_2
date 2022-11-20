7c96ab6b57a8ed525edce0a30984273de79aade8
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:45:21 +0300

    убрать пробелы в начале и конце строки

---
+++ task45.java
@@ -0,0 +1,8 @@
+public class task45 {
+    public static void main(String[] args) {
+        System.out.println("sem_2");
+
+        System.out.println("#45");
+        String g = new String(" убрать пробелы в начале и конце строки ");
+        System.out.print("ответ: "+(g.trim()));
+}}
