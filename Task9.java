41937786f19adddcb51e43a1cf02815e8b0f8439
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:38:36 +0300

    заканчивается ли строка данным значением?

---
+++ Task9.java
@@ -0,0 +1,11 @@
+public class Task9 {
+    public static void main(String[] args) {
+        System.out.println("sem_2");
+        System.out.println("#9");
+        String Str = new String("определить, заканчивается ли строка данным значением");
+        boolean result;
+        result = Str.endsWith("значением");
+        System.out.println("вывод: " + result);
+        result = Str.endsWith("ли");
+        System.out.println("вывод: " + result);
+}}
