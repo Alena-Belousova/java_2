12655d82627d12f2bb14f22caebf3ca53590c7e1
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-11-20 16:43:09 +0300

    возвращает кусок строки
    начиная с указанного индекса

---
+++ Task37.java
@@ -0,0 +1,10 @@
+public class Task37 {
+    public static void main(String[] args) {
+        System.out.println("sem_2");
+
+
+        System.out.println("#37");
+        String str = new String("возвращает кусок строки, начиная с указанного индекса");
+        System.out.println("ответ: "+(str.substring(11)));
+        System.out.println("ответ: "+(str.substring(17, 22)));
+}}
